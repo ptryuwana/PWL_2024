99f8a0e6189d4931fc534053063ae25eea561f56
Putra Nindya Yuwana <137630717+ptryuwana@users.noreply.github.com>
2024-02-22 09:20:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,74 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+use Illuminate\Support\Facades\Route; 
+ 
+Route::get('/hello', function (){
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+}); 
+
+Route::get('/welcome', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return 'Nama: Putra Nindya Yuwana | NIM: 2241720089';
+});
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+// }); 
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '. $id;
+}); 
+
+// Route::get('/user/{name?}', function ($name=null) { 
+//     return 'Nama saya '.$name; 
+// }); 
+
+// Route::get('/user/{name?}', function ($name='John') { 
+//     return 'Nama saya '.$name; 
+// });
+
+Route::get('/user/profile', function() { 
+    // return 'Halo, nama saya Putra Nindya Yuwana!';
+})->name('profile'); 
+
+// Route::middleware(['first', 'second'])->group(function () { Route::get('/', function () {
+//     // Uses first & second middleware...
+//     });
+    
+//     Route::get('/user/profile', function () {
+//     // Uses first & second middleware...
+//     });
+// });
+    
+// Route::domain('{account}.example.com')->group(function () { Route::get('user/{id}', function ($account, $id) {
+//     //
+//     });
+// });
+    
+// Route::middleware('auth')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
+
+// Route::prefix('admin')->group(function () { 
+//     Route::get('/user', [UserController::class, 'index']); 
+//     Route::get('/post', [PostController::class, 'index']); 
+//     Route::get('/event', [EventController::class, 'index']);  
+// }); 
